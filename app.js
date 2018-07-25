8812c6ce064ad920608a5bc8cf352a402ed60dbf
Joe Fearnley <joe.fearnley@gmail.com>
2018-07-24 21:58:11 -0400

    validating validation

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const Mailgun = require('mailgun-js');
 const allowedDomains = process.env.ALLOWED_DOMAINS.split(',');
-
-require('dotenv').config();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -13,8 +11,10 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
+  const host = req.get('host');
+  console.log(allowedDomains)
 
-  if (!allowedDomains.include(req.domain)) {
+  if (!allowedDomains.includes(host)) {
     res.status(422).send('Request from invalid domain');
     return;
   }
@@ -39,3 +39,7 @@ app.get('/', (req, res) => {
     console.log('Mail successfully sent!');
   });
 });
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () =>  console.log(`Server is listening on port ${port}.`));
+module.exports = server;
