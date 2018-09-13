0708aa223ac8d9927c3f05cd373aa5e28b9b9cb7
Joe Fearnley <joe.fearnley@gmail.com>
2018-09-12 22:30:09 -0400

    Moving the mail stuff into the server file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
-const Mailer = require('./mailer');
 const app = express();
-
+const apiKey = process.env.MAILGUN_API_KEY
+const domain = process.env.MAILGUN_DOMAIN;
+const mailgun = require('mailgun-js')({ apiKey, domain });;
 const allowedDomains = process.env.ALLOWED_DOMAINS.split(',');
 
 app.use((req, res, next) => {
@@ -11,7 +12,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   const host = req.get('host');
 
   if (!allowedDomains.includes(host)) {
@@ -19,8 +20,18 @@ app.get('/', (req, res) => {
     return;
   }
 
-  let mailer = new Mailer();
-  mailer.send(req.query.email, process.env.MAILGUN_TO, req.query.subject, req.query.body);
+  const data = { 
+    from: req.query.from, 
+    to: process.env.MAILGUN_TO, 
+    subject: req.query.subject, 
+    html: req.query.body 
+  };
+
+  mailgun.messages().send(data).then(function (data) {
+      res.status(500).json({ message: data });
+    }, function (err) {
+      res.status(500).json({ error: err });
+    });
 });
 
 const port = process.env.PORT || 5000;
