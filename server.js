a534837189992657b147f89ce238a0e08aaffedb
Joe Fearnley <joe.fearnley@gmail.com>
2018-09-22 09:54:02 -0400

    attempting to get this api to work

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const apiKey = process.env.MAILGUN_API_KEY
 const domain = process.env.MAILGUN_DOMAIN;
 const mailgun = require('mailgun-js')({ apiKey, domain });;
 const allowedDomains = process.env.ALLOWED_DOMAINS.split(',');
 
+app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
   const host = req.get('host');
-
-  console.log(host);
 
   if (!allowedDomains.includes(host)) {
     res.status(422).send('Request from invalid domain');
@@ -28,7 +28,7 @@ app.post('/', (req, res) => {
     from: req.query.from,
     to: process.env.MAILGUN_TO, 
     subject: req.query.subject, 
-    html: req.query.body 
+    html: req.query.messageBody
   };
 
   mailgun.messages().send(data).then(function (data) {
