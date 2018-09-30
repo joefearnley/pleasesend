1f8bc263ed3a8f4f8d3f66b113be1de661186423
Joe Fearnley <joe.fearnley@gmail.com>
2018-09-30 09:27:16 -0400

    Starting over

--- tests/mailer.spec.js
+++
@@ -1,8 +0,0 @@
-const mailer = require('../mailer');
-
-console.log(mailer);
-
-// test('sends mail', () => {
-//   let mailer = new Mailer();
-//   mailer.send('joe.fearnley@gmail.com', 'joe.fearnley@gmail.com', 'test', 'this is a test');
-// });
